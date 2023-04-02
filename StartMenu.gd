d7a2d8689277235cd3b0ecb0390f7aa0b1503940
Caroline Bell <carolinejoybell@gmail.com>
2023-04-02 01:34:31 -0700

    Update format to 3.5.1, fix start sound

--- StartMenu.gd
+++ StartMenu.gd
@@ -2,11 +2,10 @@ extends Node2D
 
 
 func _physics_process(_delta):
-	
 	if Input.is_action_just_released("ui_select"):
 		_on_Button_pressed()
 
 func _on_Button_pressed():
-	$AudioStreamPlayer2.playing = true
-	yield(get_tree().create_timer(.75),"timeout")
+	$SelectSoundPlayer.playing = true
+	yield($SelectSoundPlayer, "finished")
 	get_tree().change_scene("res://Main.tscn")
