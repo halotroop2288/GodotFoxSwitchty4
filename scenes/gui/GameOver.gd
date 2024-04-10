80c7c8f72c7b9c5ce433c311bb99357fbe8383fe
Caroline Joy Bell <halotroop2288@proton.me>
2024-04-09 19:26:32 -0500

    [Refactor] Reorganized everything
    - Reorganized every file in the project
    - Deleted duplicate files
    - Fixed issues created by this refactoring
    
    - Fixed the aspect ratio of the GUIs

--- scenes/gui/GameOver.gd
+++ scenes/gui/GameOver.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
+onready var score_label : Label = $Panel/HBoxContainer/RingsTotal
+onready var select_sound : AudioStreamPlayer = $SelectSound
 
-func _physics_process(_delta:float) -> void:
-	$Panel/RingsTotal.text = str(Global.rings)
+func _physics_process(_delta: float) -> void:
+	score_label.text = str(Global.rings)
 	if Input.is_action_just_released("ui_select"):
 		_on_Button_pressed()
 
 func _on_Button_pressed() -> void:
-	$AudioStreamPlayer.playing = true
-	#yield(get_tree().create_timer(.5),"timeout")
+	select_sound.playing = true
+	yield(select_sound, "finished")
 	get_tree().change_scene_to(Global.main_scene)
--- scenes/gui/StartMenu.gd
+++ scenes/gui/StartMenu.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
+onready var select_sound : AudioStreamPlayer = $SelectSound
+
 func _physics_process(_delta:float) -> void:
 	if Input.is_action_just_released("ui_select"):
 		_on_Button_pressed()
 
 func _on_Button_pressed() -> void:
-	$SelectSoundPlayer.playing = true
+	select_sound.play()
 	if Global.main_scene != null:
-		yield($SelectSoundPlayer, "finished")
+		yield(select_sound, "finished")
 		get_tree().change_scene_to(Global.main_scene)
